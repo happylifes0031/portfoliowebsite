9f404732cfb01dad1c773184fd0effeab625d728
Wesley <wesley@bluelabelstudio.com>
2019-05-20 18:29:59 +0200

    intro is now also functional component

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -4,41 +4,34 @@ interface IntroProps {
     togglePortfolio: () => void;
 }
 
-export class Intro extends React.Component<IntroProps> {
-    private togglePortfolio = ():void => { 
-        this.props.togglePortfolio();
-    }
-
-    public render() {
-        return (
-            <div className="content">
+export const Intro = (props:IntroProps) => {
+    return (
+        <div className="content">
             <div className="welcome">
-              <article>
+                <article>
                 <h2>Hello, i am a freelance</h2>
                 <h1>front-end developer</h1>
                 <h2>& hobby game developer</h2>
-              </article>
+                </article>
             </div>
             <div className="midSection">
-              <div className="menu">
-              <nav>
-                <a href="#portfolio" onClick={this.togglePortfolio}>Portfolio</a>
+                <div className="menu">
+                <nav>
+                <a href="#portfolio" onClick={props.togglePortfolio}>Portfolio</a>
                 <a href="mailto:e-postduif@blijlevens.nu">Contact</a>
                 <a href="../../../data/blijlevens_cv_eng.pdf" title="Download CV in English">Download C.V. <span className="downloadNote">(PDF | 1.1mb)</span></a>
-              </nav>
-              </div>
-              <div className="intro">
+                </nav>
+                </div>
+                <div className="intro">
                 <article>
-                  <p>Freelance front-end developer and hobby game developer.
+                    <p>Freelance front-end developer and hobby game developer.
                     Born and raised in Rotterdam, living in Amsterdam.
                     I have background in game development and currently do front-end development. With a love for technology and design, I have taught myself 3D design and programming!
-                  </p>
-                  <span>- Currently working for:</span> <span className='working-for'>DEVA</span>
+                    </p>
+                    <span>- Currently working for:</span> <span className='working-for'>DEVA</span>
                 </article>
-              </div>
+                </div>
             </div>
-            
-          </div>
-        )
-    }
+        </div>
+    )
 }
--- src/components/knowledgeStack/knowledgeStack.tsx
+++ src/components/knowledgeStack/knowledgeStack.tsx
@@ -24,4 +24,5 @@ export const Knowledgestack = () => {
         <Icons/>
       </div>
     </div>
-)
+  )
+}
